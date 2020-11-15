99004ba0a327a1a33be33736d9d3bad994ea1cec
Eli <epugh@stanford.edu>
2020-11-15 00:41:30 -0500

    added proto batching

---
+++ updown/data/proto_batch_sampler.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+import numpy as np
+import torch
+
+from allennlp.data import Vocabulary
+
+from updown.config import Config
+from updown.data.readers import CocoCaptionsReader, ConstraintBoxesReader, ImageFeaturesReader
+
+from updown.utils.constraints import ConstraintFilter, FiniteStateMachineBuilder
+
+
+class PrototypicalBatchSampler(object):
+    '''
+    PrototypicalBatchSampler: yield a batch of indexes at each iteration.
+    Indexes are calculated by keeping in account 'classes_per_it' and 'num_samples',
+    In fact at every iteration the batch indexes will refer to  'num_support' + 'num_query' samples
+    for 'classes_per_it' random classes.
+    __len__ returns the number of episodes per epoch (same as 'self.iterations').
+    '''
+
+    def __init__(self, vocabulary,
+                 captions_jsonpath,
+                 image_features_h5path,
+                 classes_per_it,
+                 num_samples,
+                 iterations,
+                 max_caption_length=20,
+                 in_memory=False):
+        '''
+        Initialize the PrototypicalBatchSampler object
+        Args:
+        - labels: an iterable containing all the labels for the current dataset
+        samples indexes will be infered from this iterable.
+        - classes_per_it: number of random classes for each iteration
+        - num_samples: number of samples for each iteration for each class (support + query)
+        - iterations: number of iterations (episodes) per epoch
+        '''
+        super(PrototypicalBatchSampler, self).__init__()
+        self.vocabulary = vocabulary
+        self._image_features_reader = ImageFeaturesReader(image_features_h5path, in_memory)
+        self._captions_reader = CocoCaptionsReader(captions_jsonpath)
+        self._max_caption_length = max_caption_length
+        self.classes_per_it = classes_per_it
+        self.sample_per_class = num_samples
+        self.iterations = iterations
+
+        self.classes, self.counts = np.unique(self.labels, return_counts=True)
+        self.classes = torch.LongTensor(self.classes)
+
+        # create a matrix, indexes, of dim: classes X max(elements per class)
+        # fill it with nans
+        # for every class c, fill the relative row with the indices samples belonging to c
+        # in numel_per_class we store the number of samples for each class/row
+        self.idxs = range(len(self.labels))
+        self.indexes = np.empty((len(self.classes), max(self.counts)), dtype=int) * np.nan
+        self.indexes = torch.Tensor(self.indexes)
+        self.numel_per_class = torch.zeros_like(self.classes)
+        for idx, label in enumerate(self.labels):
+            label_idx = np.argwhere(self.classes == label).item()
+            self.indexes[label_idx, np.where(np.isnan(self.indexes[label_idx]))[0][0]] = idx
+            self.numel_per_class[label_idx] += 1
+
+
+    @classmethod
+    def from_config(cls, config: Config, **kwargs):
+        r"""Instantiate this class directly from a :class:`~updown.config.Config`."""
+        _C = config
+        vocabulary = kwargs.pop("vocabulary")
+        return cls(
+            vocabulary=vocabulary,
+            captions_jsonpath=_C.DATA.TRAIN_CAPTIONS,
+            image_features_h5path=_C.DATA.TRAIN_FEATURES,
+            classes_per_it=_C.DATA.CLASSES_PER_IT,
+            num_samples=_C.DATA.NUM_SAMPLES,
+            iterations=_C.DATA.ITERATIONS,
+            max_caption_length=_C.DATA.MAX_CAPTION_LENGTH,
+            in_memory=kwargs.pop("in_memory"),
+        )
+
+    def __iter__(self):
+        '''
+        yield a batch of indexes
+        '''
+        spc = self.sample_per_class
+        cpi = self.classes_per_it
+
+        for it in range(self.iterations):
+            batch_size = spc * cpi
+            batch = torch.LongTensor(batch_size)
+            c_idxs = torch.randperm(len(self.classes))[:cpi]
+            for i, c in enumerate(self.classes[c_idxs]):
+                s = slice(i * spc, (i + 1) * spc)
+                # FIXME when torch.argwhere will exists
+                label_idx = torch.arange(len(self.classes)).long()[self.classes == c].item()
+                sample_idxs = torch.randperm(self.numel_per_class[label_idx])[:spc]
+                batch[s] = self.indexes[label_idx][sample_idxs]
+            batch = batch[torch.randperm(len(batch))]
+            yield batch
+
+    def __len__(self):
+        '''
+        returns the number of iterations (episodes) per epoch
+        '''
+        return self.iterations
